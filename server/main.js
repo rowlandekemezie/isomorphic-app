d488a59273aba5b93e0f7211f6d8bc33df4049ee
andela-rekemezie <rowland.ekemezie@andela.com>
2016-05-29 13:09:08 +0100

    Update application to avoid empty page rendering at load time

--- server/main.js
+++ server/main.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const parser = require('body-parser');
+const GloceryItem = require('./models/GloceryItem');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 const app = new express();
 const router = express.Router();
+require('babel/register');
 require('./database');
-
 app.get('/', (req, res) => {
-  res.render('./../app/index.ejs', {});
+  const application = React.createFactory(require('../app/component/GloceryItemList.jsx'));
+  GloceryItem.find((error, doc) => {
+    const generated = ReactDOMServer.renderToString(application({
+      items: doc,
+    }));
+    res.render('./../app/index.ejs', { reactOutput: generated });
+  })
 })
+
 .use(express.static(__dirname.concat('./../.tmp')))
 .use(parser.json())
 .use(parser.urlencoded({ extended: false }))
-.listen(8888);
+.listen(3000);
 require('./routes/items')(router);
 app.use('/api', router);
 module.exports = app;
